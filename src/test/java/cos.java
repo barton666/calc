5158d1933e60911b38ab046289a93c3d47cd213a
U_0220M <sakhmejanov@alfabank.ru>
2018-11-28 18:28:08 +0300

    test commit

--- src/test/java/cos.java
+++ src/test/java/cos.java
@@ -1,8 +1,19 @@
 
+import com.epam.tat.module4.Calculator;
+
+
 public class cos {
 
     public static void main(String[] args) {
+        Calculator calc = new Calculator();
+    calc.div(6,5);
+    calc.cos(145);
+    calc.mult(35, 37);
+
+    Calculator().
 
     }
+
+
 
 }
